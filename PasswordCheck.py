3da7bed6bf3670cf9c1bf58100bd90bdc75ea438
Geoff Morse <gamorse@icloud.com>
2017-08-28 00:02:17 -0700

    revised PasswordCheck to strip password doc

--- PasswordCheck.py
+++ PasswordCheck.py
@@ -1,10 +1,10 @@
-passwordFile = open('SecretPassword.txt', 'r')
-password = passwordFile.read()
+passwordFile = open('SecretPassword.txt')
+password = (passwordFile.read()).strip()
 
 access = False
-
+print password
 enteredPassword = raw_input('Enter password: ')
-
+print enteredPassword
 if enteredPassword == password:
 	access = True
 	print 'Access granted.'
