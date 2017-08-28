c1fb73113b75b49f64c307b7d20d7eef8b0ccebb
Geoff Morse <gamorse@icloud.com>
2017-08-27 21:33:12 -0700

    Initial commit

---
+++ PasswordCheck.py
@@ -0,0 +1,12 @@
+passwordFile = open('SecretPassword.txt', 'r')
+password = passwordFile.read()
+
+access = False
+
+enteredPassword = raw_input('Enter password: )
+
+if enteredPassword == password:
+	access = True
+	print 'Access granted.'
+else:
+	'DENIED'
